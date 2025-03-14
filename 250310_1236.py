f7c775a700dda05b17e5ff48f0415300aef7f823
dochinamu <gpdms6420@naver,com>
2025-03-14 15:39:02 +0900

    2025-03-14 final

--- 250310_1236.py
+++ 250310_1236.py
@@ -1,3 +1,22 @@
 import sys 
 n,m = map(int, sys.stdin.readline().split())
-print(n)
+
+security = []
+for _ in range (n):
+    security.append(sys.stdin.readline().strip())
+
+cnt = 0 
+  
+for i in security:
+    if 'X' not in i:
+        cnt += 1
+    else:
+        vertical = []
+        for j in i:
+            vertical.append(security[i][j])
+        if 
+
+
+        cnt += 1
+
+print(cnt)
--- 250314_1076.py
+++ 250314_1076.py
@@ -5,8 +5,6 @@ colors = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet"
 def resistance(c1, c2, c3):
     return (colors.index(c1) * 10 + colors.index(c2)) * (10**colors.index(c3))
 
-c1 = sys.stdin.readline().strip()
-c2 = sys.stdin.readline().strip()
-c3 = sys.stdin.readline().strip()
+c1, c2,  c3 = [sys.stdin.readline().strip() for _ in range(3)]
 
 print(resistance(c1, c2, c3))
